1e498d54419cf3e3da7a7657e773ab3bc228bd5a
smomop <63437136+smomop@users.noreply.github.com>
2024-07-16 16:01:15 +0800

    Update snell.sh

--- snell.sh
+++ snell.sh
@@ -1,38 +1,44 @@
 #!/usr/bin/env bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
+
 CONF="/etc/snell/snell-server.conf"
 SYSTEMD="/etc/systemd/system/snell.service"
-apt-get install unzip -y
+
+sudo apt-get install unzip -y
 cd ~/
 wget --no-check-certificate -O snell.zip https://dl.nssurge.com/snell/snell-server-v4.0.1-linux-amd64.zip
 unzip -o snell.zip
- rm -f snell.zip
- chmod +x snell-server
- mv -f snell-server /usr/local/bin/
- if [ -f ${CONF} ]; then
-   mkdir /etc/snell/
-   /usr/local/bin/snell-server --wizard -c ${CONF}
-   cat ${CONF}
- fi
- if [ -f ${SYSTEMD} ]; then
-   echo "Found existing service..."
-   systemctl daemon-reload
-   systemctl restart snell
- else
-   echo "Generating new service..."
-   echo "[Unit]" >>${SYSTEMD}
-   echo "Description=Snell Proxy Service" >>${SYSTEMD}
-   echo "After=network.target" >>${SYSTEMD}
-   echo "" >>${SYSTEMD}
-   echo "[Service]" >>${SYSTEMD}
-   echo "Type=simple" >>${SYSTEMD}
-   echo "LimitNOFILE=32768" >>${SYSTEMD}
-   echo "ExecStart=/usr/local/bin/snell-server -c /etc/snell/snell-server.conf" >>${SYSTEMD}
-   echo "" >>${SYSTEMD}
-   echo "[Install]" >>${SYSTEMD}
-   echo "WantedBy=multi-user.target" >>${SYSTEMD}
-   systemctl daemon-reload
-   systemctl enable snell
-   systemctl start snell
- fi
+rm -f snell.zip
+chmod +x snell-server
+sudo mv -f snell-server /usr/local/bin/
+
+if [ ! -f ${CONF} ]; then
+  sudo mkdir -p /etc/snell/
+  sudo /usr/local/bin/snell-server --wizard -c ${CONF}
+  cat ${CONF}
+fi
+
+if [ -f ${SYSTEMD} ]; then
+  echo "Found existing service..."
+  sudo systemctl daemon-reload
+  sudo systemctl restart snell
+else
+  echo "Generating new service..."
+  sudo tee ${SYSTEMD} > /dev/null <<EOL
+[Unit]
+Description=Snell Proxy Service
+After=network.target
+
+[Service]
+Type=simple
+LimitNOFILE=32768
+ExecStart=/usr/local/bin/snell-server -c /etc/snell/snell-server.conf
+
+[Install]
+WantedBy=multi-user.target
+EOL
+  sudo systemctl daemon-reload
+  sudo systemctl enable snell
+  sudo systemctl start snell
+fi
