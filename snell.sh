cf83f4f125dab95f54de2f3be319c5d3c5b67af0
smomop <63437136+smomop@users.noreply.github.com>
2024-07-16 16:06:49 +0800

    Update snell.sh

--- snell.sh
+++ snell.sh
@@ -5,27 +5,26 @@ export PATH
 CONF="/etc/snell/snell-server.conf"
 SYSTEMD="/etc/systemd/system/snell.service"
 
-sudo apt-get install unzip -y
-cd ~/
+apt-get install unzip -y
 wget --no-check-certificate -O snell.zip https://dl.nssurge.com/snell/snell-server-v4.0.1-linux-amd64.zip
 unzip -o snell.zip
 rm -f snell.zip
 chmod +x snell-server
-sudo mv -f snell-server /usr/local/bin/
+mv -f snell-server /usr/local/bin/
 
 if [ ! -f ${CONF} ]; then
-  sudo mkdir -p /etc/snell/
-  sudo /usr/local/bin/snell-server --wizard -c ${CONF}
+  mkdir -p /etc/snell/
+  /usr/local/bin/snell-server --wizard -c ${CONF}
   cat ${CONF}
 fi
 
 if [ -f ${SYSTEMD} ]; then
   echo "Found existing service..."
-  sudo systemctl daemon-reload
-  sudo systemctl restart snell
+  systemctl daemon-reload
+  systemctl restart snell
 else
   echo "Generating new service..."
-  sudo tee ${SYSTEMD} > /dev/null <<EOL
+  tee ${SYSTEMD} > /dev/null <<EOL
 [Unit]
 Description=Snell Proxy Service
 After=network.target
@@ -38,7 +37,7 @@ ExecStart=/usr/local/bin/snell-server -c /etc/snell/snell-server.conf
 [Install]
 WantedBy=multi-user.target
 EOL
-  sudo systemctl daemon-reload
-  sudo systemctl enable snell
-  sudo systemctl start snell
+  systemctl daemon-reload
+  systemctl enable snell
+  systemctl start snell
 fi
